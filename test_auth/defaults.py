b6b473cd965b953c041aa61b3da39d228f943ffc
Aurélien Bompard <aurelien@bompard.org>
2020-10-21 17:42:52 +0200

    Add the fas attributes scope
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- test_auth/defaults.py
+++ test_auth/defaults.py
@@ -6,12 +6,21 @@ OIDC_SCOPES = [
     "profile",
     "https://id.fedoraproject.org/scope/groups",
     "https://id.fedoraproject.org/scope/agreements",
+    "https://id.fedoraproject.org/scope/fas-attributes",
 ]
 
 # OpenID
 OPENID_ENDPOINT = "https://id.fedoraproject.org/openid/"
 OPENID_ASK_FOR = ["email", "nickname", "fullname"]
-OPENID_ASK_FOR_OPTIONAL = ["language", "timezone", "website", "blog", "image"]
+OPENID_ASK_FOR_OPTIONAL = [
+    "language",
+    "timezone",
+    "website",
+    "blog",
+    "image",
+    "ssh_key",
+    "gpg_keyid",
+]
 
 
 # FAS OpenID
